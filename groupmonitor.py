d241d7b524935cbeb5a5e5fb45f2238a4dd2e8d4
Monsterovich <Monsterovich@gmail.com>
2016-09-17 18:03:48 +0200

    Generate first line for our stats

--- groupmonitor.py
+++ groupmonitor.py
@@ -7,6 +7,7 @@ import urllib2
 from datetime import datetime
 import csv
 import sys
+import os
 
 def update_stats(link, filename):
     response = urllib2.urlopen(link)
@@ -16,6 +17,8 @@ def update_stats(link, filename):
         return int(re.search(regex, html).group(1))
         
     with open(filename, 'ab') as file:
+        if os.stat(filename).st_size == 0:
+            file.write('timestamp,followers,posts,tracks,playlists,plays,likes,reposts\n')
         row = [
             datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
             get('"followers_count":(\d+)'),
@@ -31,7 +34,6 @@ def update_stats(link, filename):
         file.write(','.join(map(str, row)) + '\n')
         
 if __name__ == '__main__':
-    import sys
     if len(sys.argv) != 3:
         print 'Usage: groupmonitor.py <track url> <output>'
         sys.exit(2)
