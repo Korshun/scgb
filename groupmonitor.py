cacf7ac7035d6928251c5f3a2d27dea41c646ce3
Monsterovich <Monsterovich@gmail.com>
2016-09-17 17:53:48 +0200

    Replaced tabs with spaces

--- groupmonitor.py
+++ groupmonitor.py
@@ -9,32 +9,32 @@ import csv
 import sys
 
 def update_stats(link, filename):
-	response = urllib2.urlopen(link)
-	html = response.read()
+    response = urllib2.urlopen(link)
+    html = response.read()
 
-	def get(regex):
-		return int(re.search(regex, html).group(1))
-		
-	with open(filename, 'ab') as file:
-		row = [
-			datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
-			get('"followers_count":(\d+)'),
-			get('Total posts:\s*(\d+)'),
-			get('Tracks posted:\s*(\d+)'),
-			get('Playlists posted:\s*(\d+)'),
-			get('"playback_count":(\d+)'),
-			get('"likes_count":(\d+),"permalink"'),
-			get('"reposts_count":(\d+),"secret_token"'),
-		]
-		
-		print(row)
-		file.write(','.join(map(str, row)) + '\n')
-		
+    def get(regex):
+        return int(re.search(regex, html).group(1))
+        
+    with open(filename, 'ab') as file:
+        row = [
+            datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+            get('"followers_count":(\d+)'),
+            get('Total posts:\s*(\d+)'),
+            get('Tracks posted:\s*(\d+)'),
+            get('Playlists posted:\s*(\d+)'),
+            get('"playback_count":(\d+)'),
+            get('"likes_count":(\d+),"permalink"'),
+            get('"reposts_count":(\d+),"secret_token"'),
+        ]
+        
+        print(row)
+        file.write(','.join(map(str, row)) + '\n')
+        
 if __name__ == '__main__':
-	import sys
-	if len(sys.argv) != 3:
-		print 'Usage: groupmonitor.py <track url> <output>'
-		sys.exit(2)
-		
-	print 'Retrieving:', 
-	update_stats(sys.argv[1], sys.argv[2])
+    import sys
+    if len(sys.argv) != 3:
+        print 'Usage: groupmonitor.py <track url> <output>'
+        sys.exit(2)
+        
+    print 'Retrieving:', 
+    update_stats(sys.argv[1], sys.argv[2])
