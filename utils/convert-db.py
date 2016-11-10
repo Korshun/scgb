268854ff8cafe9b9e1042e71e8ad05c6161683a0
Alexander Korshun <Korshun@users.noreply.github.com>
2016-11-11 01:58:51 +0300

    Add convert-db.py

---
+++ utils/convert-db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sqlite3
+import os
+
+from sys import argv, exit
+from scgb.database import Database
+
+def convert_db(inputfile, outputfile):
+	input = sqlite3.connect(inputfile)
+	if os.path.exists(outputfile):
+		print('Output database already exists')
+		exit(1)
+		
+	db = Database(outputfile)
+	
+	track_count = input.execute("SELECT value FROM SCGB WHERE name='track_count'").fetchone()[0]
+	playlist_count = input.execute("SELECT value FROM SCGB WHERE name='playlist_count'").fetchone()[0]
+	
+	db.sqlite.execute("UPDATE ResourceCounts SET count=? WHERE resource_type='track'", (track_count,))
+	db.sqlite.execute("UPDATE ResourceCounts SET count=? WHERE resource_type='playlist'", (playlist_count,))
+	db.commit()
+	
+	
+if __name__ == '__main__':
+	if len(argv) != 3:
+		print('Usage: convert-db.py <input database> <output database>')
+	
+	convert_db(argv[1], argv[2])
