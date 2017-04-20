5604b2795e0e44890e291dd3fc44ab0c1de16322
Alexander Korshun <Korshun@users.noreply.github.com>
2017-04-20 17:28:31 +0300

    Move log init to main

--- scgb.py
+++ scgb.py
@@ -2,6 +2,14 @@
 
 from scgb.main import bot_init, check_comments
     
+import sys
+import logging
+	
 if __name__ == '__main__':
+    # Init log
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, datefmt='[%Y-%m-%d %H:%M:%S]', format='%(asctime)s %(levelname)s %(message)s')
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+	
     bot_init()
     check_comments()
--- scgb/main.py
+++ scgb/main.py
@@ -31,11 +31,6 @@ def bot_init():
     global db
     global config
     
-    # Init log
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO, datefmt='[%Y-%m-%d %H:%M:%S]', format='%(asctime)s %(levelname)s %(message)s')
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    
     # Init config
     if len(sys.argv) > 1:
         config = imp.load_source('scgb_config', sys.argv[1])
